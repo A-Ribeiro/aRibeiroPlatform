1f5be4ab78ba1ac265c5473e809fb54a00a5302e
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2022-02-07 00:04:31 -0300

    added high resolution sleep to windows builds

--- src/Platform/PlatformSleep.cpp
+++ src/Platform/PlatformSleep.cpp
@@ -1,9 +1,26 @@
 #include "PlatformSleep.h"
 #include "PlatformTime.h"
 
-
+#if defined(OS_TARGET_win)
+#include <timeapi.h>
+#endif
 
 namespace aRibeiro {
+#if defined(OS_TARGET_win)
+    class UseWindowsHighResolutionClock {
+    public:
+        UseWindowsHighResolutionClock() {
+            timeBeginPeriod(1);
+        }
+        ~UseWindowsHighResolutionClock() {
+            timeEndPeriod(1);
+        }
+        static UseWindowsHighResolutionClock* Instance() {
+            static UseWindowsHighResolutionClock useWindowsHighResolutionClock;
+            return &useWindowsHighResolutionClock;
+        }
+    };
+#endif
 
     void PlatformSleep::sleepSec(int secs) {
         sleepMillis(secs * 1000);
@@ -11,6 +28,7 @@ namespace aRibeiro {
 
     void PlatformSleep::sleepMillis(int millis) {
 #if defined(OS_TARGET_win)
+        UseWindowsHighResolutionClock::Instance();
         Sleep(millis);
 #else
         usleep(millis * 1000);
