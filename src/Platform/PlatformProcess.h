64134c9a58170ac543e6731decab2913bd3638ca
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2022-03-09 17:09:19 -0300

    fixed path arguments to create process on windows

--- src/Platform/PlatformProcess.h
+++ src/Platform/PlatformProcess.h
@@ -6,22 +6,24 @@
 #include <aRibeiroPlatform/PlatformMutex.h>
 #include <aRibeiroPlatform/PlatformAutoLock.h>
 #include <aRibeiroPlatform/PlatformThread.h>
+#include <aRibeiroPlatform/PlatformPath.h>
 #include <string>
+#include <algorithm>
 #include <signal.h>
 
 namespace aRibeiro {
 
 #if defined(_WIN32)
-//
-// This Class is needed only in windows systems.... 
-// that does not have a way to signaling a process...
-//
+    //
+    // This Class is needed only in windows systems.... 
+    // that does not have a way to signaling a process...
+    //
     class PlatformProcessGracefulWindowsProcessTerminator {
 
         DWORD dwProcessId;
         std::string pid_str;
 
-        PlatformLowLatencyQueueIPC *queue;
+        PlatformLowLatencyQueueIPC* queue;
 
         PlatformThread thread;
 
@@ -44,9 +46,9 @@ namespace aRibeiro {
         }
 
     public:
-        PlatformProcessGracefulWindowsProcessTerminator(void(*_fnc)(int)):
-                thread(this, &PlatformProcessGracefulWindowsProcessTerminator::threadRun),
-                fnc(_fnc) {
+        PlatformProcessGracefulWindowsProcessTerminator(void(*_fnc)(int)) :
+            thread(this, &PlatformProcessGracefulWindowsProcessTerminator::threadRun),
+            fnc(_fnc) {
             queue = NULL;
             dwProcessId = GetCurrentProcessId();
 
@@ -54,7 +56,7 @@ namespace aRibeiro {
             sprintf(aux, "p%u", dwProcessId);
             pid_str = aux;
 
-            queue = new PlatformLowLatencyQueueIPC(pid_str.c_str(),PlatformQueueIPC_WRITE, 16, sizeof(int) );
+            queue = new PlatformLowLatencyQueueIPC(pid_str.c_str(), PlatformQueueIPC_WRITE, 16, sizeof(int));
             thread.setShouldDisposeThreadByItself(true);
             thread.start();
         }
@@ -81,10 +83,22 @@ namespace aRibeiro {
 
         int force_horrible_terminate_after_ms;
 
-    #if defined(_WIN32)
+#if defined(_WIN32)
         STARTUPINFO startupInfo;
         PROCESS_INFORMATION processInformation;
-    #endif
+#endif
+
+        static void findAndReplaceAll(std::string* data, const std::string& toSearch, const std::string& replaceStr) {
+            // Get the first occurrence
+            size_t pos = data->find(toSearch);
+            // Repeat till end is reached
+            while (pos != std::string::npos) {
+                // Replace this occurrence of Sub String
+                data->replace(pos, toSearch.size(), replaceStr);
+                // Get the next occurrence from the current position
+                pos = data->find(toSearch, pos + replaceStr.size());
+            }
+        }
 
     public:
         PlatformProcess(const std::string& _lpApplicationName, const std::string& _commandLine, int _force_horrible_terminate_after_ms = 5000) {
@@ -95,8 +109,15 @@ namespace aRibeiro {
             commandLine = _commandLine;
             force_horrible_terminate_after_ms = _force_horrible_terminate_after_ms;
 
-        #if defined(_WIN32)
+#if defined(_WIN32)
+
+            findAndReplaceAll(&lpApplicationName, "/", PlatformPath::SEPARATOR);
             lpApplicationName += ".exe";
+
+            if (commandLine.length() > 0)
+                commandLine = lpApplicationName + " " + commandLine;
+            else
+                commandLine = lpApplicationName;
 
             // set the size of the structures
             ZeroMemory(&startupInfo, sizeof(startupInfo));
@@ -116,13 +137,13 @@ namespace aRibeiro {
                 &processInformation             // Pointer to PROCESS_INFORMATION structure (removed extra parentheses)
             );
 
-            if (isCreated()){
+            if (isCreated()) {
                 char aux[64];
                 sprintf(aux, "p%u", processInformation.dwProcessId);
                 pid_str = aux;
             }
 
-        #endif
+#endif
 
         }
 
@@ -148,32 +169,32 @@ namespace aRibeiro {
         int signal(int sig = SIGINT) {
             PlatformAutoLock autoLock(&mutex);
 
-            if (isCreated()){
-                #if defined(_WIN32)
+            if (isCreated()) {
+#if defined(_WIN32)
 
-                    //check the process signal queue
-                    PlatformLowLatencyQueueIPC queue(pid_str.c_str(),PlatformQueueIPC_WRITE, 16, sizeof(int) );
-                    queue.write( (uint8_t *)&sig, sizeof(int), false );
+                //check the process signal queue
+                PlatformLowLatencyQueueIPC queue(pid_str.c_str(), PlatformQueueIPC_WRITE, 16, sizeof(int));
+                queue.write((uint8_t*)&sig, sizeof(int), false);
 
-                    if (WaitForSingleObject(processInformation.hProcess, force_horrible_terminate_after_ms) == WAIT_TIMEOUT){
-                        // if the process did not finish in the time
-                        //   Forcibly terminate the process...
-                        TerminateProcess(processInformation.hProcess, 0);
-                    }
+                if (WaitForSingleObject(processInformation.hProcess, force_horrible_terminate_after_ms) == WAIT_TIMEOUT) {
+                    // if the process did not finish in the time
+                    //   Forcibly terminate the process...
+                    TerminateProcess(processInformation.hProcess, 0);
+                }
 
-                    DWORD code;
-                    GetExitCodeProcess(processInformation.hProcess, &code);
+                DWORD code;
+                GetExitCodeProcess(processInformation.hProcess, &code);
 
-                    CloseHandle(processInformation.hProcess);
-                    CloseHandle(processInformation.hThread);
+                CloseHandle(processInformation.hProcess);
+                CloseHandle(processInformation.hThread);
 
-                    //ARIBEIRO_ABORT(code != 0, "Child process terminated with code: %u", code);
-                    exit_code = code;
+                //ARIBEIRO_ABORT(code != 0, "Child process terminated with code: %u", code);
+                exit_code = code;
 
-                    pid_str = "";
-                    process_created = false;
-                    
-                #endif
+                pid_str = "";
+                process_created = false;
+
+#endif
             }
 
             return exit_code;
